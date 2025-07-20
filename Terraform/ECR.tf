c21c85d3546149f39287d8fa71c2302104c7edc7
ali hussein <aliashmawy595@gmail.com>
2025-07-20 18:28:01 +0300

    Deleted ECR.tf

--- Terraform/ECR.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecr_repository" "backend" {
-  name                 = "backend"
-  image_tag_mutability = "MUTABLE"
-}
-
-resource "aws_ecr_repository" "frontend" {
-  name                 = "frontend"
-  image_tag_mutability = "MUTABLE"
-}
